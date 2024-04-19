ffed9a4f8df5cedaf4d254ae081bb48d28c1ff26
IgorAngelotti <lanzaigor@live.com>
2024-04-19 11:57:28 -0300

    scriptBot correção

--- script/bot_codesf.js
+++ script/bot_codesf.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $r2 = $r1.shadowRoot.querySelector("df-messenger-chat");
     $r3 = $r2.shadowRoot.querySelector("df-messenger-user-input"); //for other mods
     var sheet = new CSSStyleSheet;
-    sheet.replaceSync( div.chat-wrapper[opened="true"], { height: 500px; border:2px solid rgba(0, 0, 0, 0.5); border-radius: 7px;});
+    sheet.replaceSync( `div.chat-wrapper[opened="true"] { height: 500px; border:2px solid rgba(0, 0, 0, 0.5); border-radius: 7px;}`);
     $r2.shadowRoot.adoptedStyleSheets = [ sheet ];
-    });
-}); 
+});
+});
