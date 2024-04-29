c717f17d6ef5dafbcde3fbd0bec669fc6e67ae68
LucasC2 <lucasccamposs01@outlook.com>
2024-04-29 00:13:39 -0300

    criação da área de colaboradores

--- script/script.js
+++ script/script.js
@@ -33,3 +33,28 @@ else{
   document.querySelector("nav ul").style.display="none"
 }
 }
+
+/*Partners*/
+
+$(document).ready(function(){
+  $('.partners-logos').slick({
+     slidesToShow: 5,
+     slidesToScroll: 1,
+     autoplay: true,
+     autoplaySpeed: 2000,
+     arrows: false,
+     dots: false,
+     pauseOnHover: false,
+     responsive: [{
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 4
+        }
+     }, {
+         breakpoint: 520,
+         settings: {
+             slidesToShow: 3
+         }
+     }] 
+  })
+})
